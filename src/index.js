1f362c1220934e4980bfa70e55f17ff93c4fb019
Harley Ferrer Lumagui <syntx.mail@gmail.com>
2022-01-20 16:31:22 +0800

    Implement Inline Style

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import reactDom from 'react-dom'
 import './index.css'
 
 function App() {
+
+    const paraStyle = {
+        "marginBottom": "15px"
+    };
+
     return (
         <div className="app">
             <header className="app-header">
                 <h1>My Amazing ToDo-List App</h1>
-                <p>The most simple amd amazing todo-list.</p>
+                <p style={ paraStyle }>The most simple amd amazing todo-list.</p>
+                <ul>
+                    <li className="tasks-item">Follow Edukasyon.ph on Facebook.</li>
+                    <li className="tasks-item">Follow AWS Siklab Pilipinas on Facebook.</li>
+                    <li className="tasks-item">Follow Zuitt Coding Bootcamp on Facebook.</li>
+                </ul>
             </header>
         </div>
     );
